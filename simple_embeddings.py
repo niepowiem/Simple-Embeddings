6fd2c2c139c53348922d090fd006a6f723b08807
Bartosz Zieliński <59732574+niepowiem@users.noreply.github.com>
2025-05-13 16:55:52 +0200

    Update simple_embeddings.py

--- simple_embeddings.py
+++ simple_embeddings.py
@@ -661,27 +661,3 @@ class EmbeddingData:
             embed += [[0] * self.d_model] * (context - len(embed))
 
         return embed
-
-data = ['He is fast as lightning', "I'm learning how to run as fast as him"]
-
-# === BPE ===
-
-# bpe = BytePairEncoder(corpus=data, white_space=True)
-# print(bpe.vocabulary)
-# 
-# embeddings = EmbeddingData(vocabulary=bpe.vocabulary)
-# embeddings.calculate(bpe.tokenize(data), smoothing=3e-4, d_model=128, window_size=3)
-# 
-# embedded_sequence = embeddings.embed_sequence(bpe.tokenize("He's learning how to run"))
-# print(embedded_sequence)
-
-# === WordPiece ===
-
-# wp = WordPiece(corpus=data)
-# print(wp.vocabulary)
-# 
-# embeddings = EmbeddingData(vocabulary=wp.vocabulary)
-# embeddings.calculate(wp.tokenize(data), smoothing=3e-4, d_model=128, window_size=3)
-# 
-# embedded_sequence = embeddings.embed_sequence(wp.tokenize("He's learning how to run"))
-# print(embedded_sequence)
